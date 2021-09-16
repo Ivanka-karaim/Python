2c2a4f9bc1cd5a86ac48522844a22518fe932858
Іванка <@DESKTOP-0K94GLI>
2021-09-16 11:06:56 +0300

    Code

--- lab1/task3.py
+++ lab1/task3.py
@@ -4,22 +4,24 @@ Number = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
 
 def check(index):
     if (
-        (expression[index] not in Number and expression[index] not in Sign) or
-        (expression[index-1] not in Number and expression[index] == '0' and expression[index] in Number) or
-        (expression[index] in Sign and expression[index+1] in Sign)
+            (expression[index] not in Number and expression[index] not in Sign) or
+            (expression[index - 1] not in Number and expression[index] == '0' and expression[index] in Number) or
+            (expression[index] in Sign and expression[index + 1] in Sign)
     ):
         return False, None
-    if index == len(expression)-2:
-        if expression[index+1] not in Number and expression[index+1] not in Sign:
+    if index == len(expression) - 2:
+        if expression[index + 1] not in Number and expression[index + 1] not in Sign:
             return False, None
         else:
             return True, eval(expression)
-    return check(index+1)
+    return check(index + 1)
 
 
 expression = str(input("Enter expression: "))
-if ((expression == "") or (expression[0] not in Number and expression[0] != '-') or
-    (expression[len(expression) - 1] not in Number)):
+if (
+        (expression == "") or (expression[0] not in Number and expression[0] != '-') or
+        (expression[len(expression) - 1] not in Number)
+):
     print((False, None))
     exit(1)
 print(check(1))
