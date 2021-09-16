caad915ad0e07afab9817f2e566b99f2e027c931
Іванка <@DESKTOP-0K94GLI>
2021-09-16 11:04:47 +0300

    Code

--- lab1/task3.py
+++ lab1/task3.py
@@ -5,7 +5,7 @@ Number = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
 def check(index):
     if (
         (expression[index] not in Number and expression[index] not in Sign) or
-        (expression[index] == '0' and expression[index+1] in Number) or
+        (expression[index-1] not in Number and expression[index] == '0' and expression[index] in Number) or
         (expression[index] in Sign and expression[index+1] in Sign)
     ):
         return False, None
@@ -18,7 +18,7 @@ def check(index):
 
 
 expression = str(input("Enter expression: "))
-if (expression == "") or (expression[0] in Sign) or (expression[len(expression) - 1] in Sign):
+if (expression == "") or (expression[0] not in Number and expression[0] != '-') or (expression[len(expression) - 1] not in Number):
     print((False, None))
     exit(1)
-print(check(0))
+print(check(1))
