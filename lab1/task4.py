6d27289e7094b65b22a91c725b71f0ffe9c0a282
Іванка <@DESKTOP-0K94GLI>
2021-09-16 11:10:07 +0300

    Code

--- lab1/task4.py
+++ lab1/task4.py
@@ -9,11 +9,11 @@ Bars = [int(item) for item in args.bars]
 length = len(Bars)
 Matrix = [[0 for j in range(args.capacity + 1)] for i in range(length + 1)]
 for i in range(length + 1):
-      for j in range(args.capacity + 1):
-            if i == 0 or j == 0:
-                  Matrix[i][j] = 0
-            elif Bars[i - 1] <= j:
-                  Matrix[i][j] = max(Bars[i - 1] + Matrix[i - 1][j - Bars[i - 1]], Matrix[i - 1][j])
-            else:
-                  Matrix[i][j] = Matrix[i - 1][j]
+    for j in range(args.capacity + 1):
+        if i == 0 or j == 0:
+            Matrix[i][j] = 0
+        elif Bars[i - 1] <= j:
+            Matrix[i][j] = max(Bars[i - 1] + Matrix[i - 1][j - Bars[i - 1]], Matrix[i - 1][j])
+        else:
+            Matrix[i][j] = Matrix[i - 1][j]
 print("Max: ", Matrix[len(Bars)][args.capacity])
