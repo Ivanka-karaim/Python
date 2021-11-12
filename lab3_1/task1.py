f185c9a8f622e523fc52ede3aed750ded1dd93d5
Іванка <@DESKTOP-0K94GLI>
2021-11-12 17:03:09 +0200

    Code 1,2,3,4

---
+++ lab3_1/task1.py
@@ -0,0 +1,278 @@
+import json
+from datetime import datetime, timedelta
+
+
+class BaseTicket:
+    """
+    Base Ticket
+    Attributes
+    __________
+    num_ticket: str
+        id ticket
+    name: str
+        name event
+    price: int, float
+        price ticket
+    seat: int
+        seat event
+    date: str
+        date event
+
+    Methods
+    _______
+    dict():
+        returns the created dictionary with ticket data
+    """
+    def __init__(self, num_ticket, name, price, seat, date):
+        """
+        :param num_ticket:
+            id ticket
+        :param name:
+            name event
+        :param price:
+            price ticket
+        :param seat:
+            seat for the event
+        :param date:
+            date event
+        """
+        self.num_ticket = num_ticket
+        self.name = name
+        self.price = price
+        self.seat = seat
+        self.date = date
+
+    @property
+    def num_ticket(self):
+        return self.__num_ticket
+
+    @num_ticket.setter
+    def num_ticket(self, value):
+        if not isinstance(value, str):
+            raise TypeError
+        if not value.isdigit():
+            raise ValueError
+        self.__num_ticket = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError
+        self.__name = value
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        self.__price = value
+
+    @property
+    def seat(self):
+        return self.__seat
+
+    @seat.setter
+    def seat(self, value):
+        if not isinstance(value, int):
+            raise TypeError
+        self.__seat = value
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, value):
+        if not isinstance(value, str):
+            raise TypeError
+        self.__date = value
+
+    def __str__(self):
+        return f'Ticket\nInvention:{self.name} Price: {self.price} Seat: {self.seat}\n' \
+               f'Date: {self.date} Code: {self.num_ticket}'
+
+    def dict(self):
+        """
+        Dict base ticket
+        :return: dict describing the base ticket
+        """
+        return {"id": self.num_ticket, "name": self.name, "price": self.price, "seat": self.seat, "date": self.date,
+                "type": "BaseTicket"}
+
+
+class AdvanceTicket(BaseTicket):
+    """
+    The class describes advance ticket
+    """
+    def __init__(self, num_ticket, name, price, seat, date):
+        super().__init__(num_ticket, name, price*0.6, seat, date)
+
+    def __str__(self):
+        return f'Advance ticket\nInvention:{self.name} Price: {self.price} Seat: {self.seat}\n' \
+               f'Date: {self.date} Code: {self.num_ticket}'
+
+    def dict(self):
+        """
+        Dict advance ticket
+        :return: dict describing the advance ticket
+        """
+        return {"id": self.num_ticket, "name": self.name, "price": self.price, "seat": self.seat, "date": self.date,
+                "type": "AdvanceTicket"}
+
+
+class LateTicket(BaseTicket):
+    """
+    The class describes late ticket
+    """
+    def __init__(self, num_ticket, name, price, seat, date):
+        super().__init__(num_ticket, name, price*1.1, seat, date)
+
+    def __str__(self):
+        return f'Late ticket\nInvention:{self.name} Price: {self.price} Seat: {self.seat}\n' \
+               f'Date: {self.date} Code: {self.num_ticket}'
+
+    def dict(self):
+        """
+        Dict late ticket
+        :return: dict describing the late ticket
+        """
+        return {"id": self.num_ticket, "name": self.name, "price": self.price, "seat": self.seat, "date": self.date,
+                "type": "LateTicket"}
+
+
+class StudentTicket(BaseTicket):
+    """
+    The class describes student ticket
+    """
+    def __init__(self, num_ticket, name, price, seat, date):
+        super().__init__(num_ticket, name, price*0.5, seat, date)
+
+    def __str__(self):
+        return f'Student ticket\nInvention:{self.name} Price: {self.price} Seat: {self.seat}\n' \
+               f'Date: {self.date} Code: {self.num_ticket}'
+
+    def dict(self):
+        """
+        Dict student ticket
+        :return: dict describing the student ticket
+        """
+        return {"id": self.num_ticket, "name": self.name, "price": self.price, "seat": self.seat, "date": self.date,
+                "type": "StudentTicket"}
+
+
+def buy_ticket(code, seat, ind=None):
+    """
+    The function with which to buy a ticket
+    :param code:
+        code event
+    :param seat:
+        seat for the event
+    :param ind:
+        student indicator
+    :return: the number of the purchased ticket
+    """
+    if len(str(code)) != 4:
+        print("The event code was entered incorrectly")
+        return None
+    with open("ticket_already_bought.json", 'r') as read_file:
+        purchased_tickets = json.load(read_file)
+    for purchased_ticket in purchased_tickets:
+        if purchased_ticket["id"] == str(code) + str(seat):
+            print("The place at the back event is taken")
+            return None
+    with open("events.json", 'r') as file:
+        events = json.load(file)
+    data = None
+    for event in events:
+        if event["code"] == str(code):
+            data = event
+    if not data:
+        print("The event code was entered incorrectly")
+        return None
+    if ind:
+        ticket = StudentTicket(str(code)+str(seat), data["name"], data["price"], seat, data["date"])
+        purchased_tickets.append(ticket.dict())
+    else:
+        date = datetime.strptime(data["date"], "%y-%m-%d")
+        time = date - datetime.today()
+        if time > timedelta(days=60):
+            ticket = AdvanceTicket(str(code)+str(seat), data["name"], data["price"], seat, data["date"])
+            purchased_tickets.append(ticket.dict())
+        elif time < timedelta(days=10):
+            ticket = LateTicket(str(code)+str(seat), data["name"], data["price"], seat, data["date"])
+            purchased_tickets.append(ticket.dict())
+        elif time < timedelta(days=0):
+            print("This event has already taken place")
+            return None
+        else:
+            ticket = BaseTicket(str(code)+str(seat), data["name"], data["price"], seat, data["date"])
+            purchased_tickets.append(ticket.dict())
+    with open("ticket_already_bought.json", 'w') as write_file:
+        json.dump(purchased_tickets, write_file, indent=4)
+    return str(code)+str(seat)
+
+
+def get_ticket(number):
+    """
+    The function that holds the ticket for it id
+    :param number:
+        id ticket
+    :return: ticket
+    """
+    with open("ticket_already_bought.json", 'r') as file:
+        tickets = json.load(file)
+    gets = None
+    for ticket in tickets:
+        if ticket["id"] == number:
+            gets = ticket
+
+    if not gets:
+        print("No such ticket found")
+        return None
+    if gets["type"] == "BaseTicket":
+        return BaseTicket(gets["id"], gets["name"], gets["price"], gets["seat"], gets["date"])
+    if gets["type"] == "LateTicket":
+        return LateTicket(gets["id"], gets["name"], gets["price"]/1.1, gets["seat"], gets["date"])
+    if gets["type"] == "StudentTicket":
+        return StudentTicket(gets["id"], gets["name"], gets["price"]/0.5, gets["seat"], gets["date"])
+    if gets["type"] == "AdvanceTicket":
+        return AdvanceTicket(gets["id"], gets["name"], gets["price"]/0.6, gets["seat"], gets["date"])
+
+
+def display_all_events():
+    with open("events.json", 'r') as file:
+        events = json.load(file)
+    for event in events:
+        print("Code: " + event["code"]+" Name event: " + event["name"] +
+              " Price: " + str(event["price"]) + " Date: " + event["date"])
+
+
+display_all_events()
+try:
+    print("1 - Buy a ticket\n2 - Get a ticket\n")
+    mod = int(input("Enter: "))
+    if mod == 1:
+        code = int(input("Select an event(enter the event code): "))
+        seat = int(input("Enter seat: "))
+        student = int(input("Are you a student?( 1- yes, 0- no): "))
+        tick = buy_ticket(code, seat, student)
+        if tick:
+            print("Your id: " + tick)
+    if mod == 2:
+        idd = int(input("Enter id ticket: "))
+        my_ticket = get_ticket(str(idd))
+        if my_ticket:
+            print(my_ticket)
+except (ValueError, FileNotFoundError):
+    print("Incorrectly entered data")
+
+
---
+++ lab3_1/task2.py
@@ -0,0 +1,401 @@
+import json
+from datetime import datetime
+
+
+def serialization():
+    data = [{"day": "Monday", "name": "Margarita", "price": 100,
+             "ingredients": ["tomato sauce", "cheese", "tomatoes", "basil"]},
+            {"day": "Tuesday", "name": "Karbonara", "price": 120, "ingredients":
+                ["cream sauce", "cheese", "bacon", "eggs", "onions", "garlic", "basil"]},
+            {"day": "Wednesday", "name": "Four cheeses",
+             "price": 110, "ingredients": ["mozzarella", "parmesan", "dor blue", "cheddar"]},
+            {"day": "Thursday", "name": "Hawaiian",
+             "price": 110, "ingredients": ["cheese", "tomato base", "bacon", "pineapple"]},
+            {"day": "Friday", "name": "Bavarian", "price": 130,
+             "ingredients": ["bacon", "hunting sausages", "salami", "ham", "sauce"]},
+            {"day": "Saturday", "name": "Pepperoni",
+             "price": 100,
+             "ingredients": ["mozzarella", "russian cheese", "pepperoni", "tomatoes", "basil", "tomato sauce",
+                             "chili"]},
+            {"day": "Sunday", "name": "Mexican", "price": 120,
+             "ingredients": ["salami", "mushrooms", "tomatoes", "sauce", "cheese", "parsley"]
+             }]
+    with open("pizza_of_the_day.json", 'w') as f:
+        json.dump(data, f, indent=4)
+    data1 = {"mustard": 10, "cucumbers": 15, "corn": 5, "french fries": 6, "olives": 20, "mushrooms": 17}
+    with open("pizza_topings.json", 'w') as file:
+        json.dump(data1, file, indent=4)
+
+
+class PizzaOfTheDay:
+    """
+    Basic pizza class
+    Attributes
+    __________
+    day: str
+        day week
+    name: str
+        Name pizza
+    price: int, float
+        price pizza
+    ingredients: str
+        ingredients pizza
+    toppings:
+        toppings pizza
+
+    Methods
+    _______
+    prices():
+        returns a whole pizza with extra ingredients
+    add_ingredient():
+        adds ingredients to pizza
+    del_ingredient():
+        removes ingredients from pizza
+    """
+    def __init__(self, day, pizza_of_a_day, price, ingredients, toppings=None):
+        """
+        :param day:
+            day week
+        :param pizza_of_a_day:
+            pizza_of_the_day
+        :param price:
+            price pizza
+        :param ingredients:
+            ingredients pizza
+        :param toppings:
+            toppings pizza
+        """
+        self.day = day
+        self.name = pizza_of_a_day
+        self.price = price
+        self.ingredients = ingredients
+        if not toppings:
+            self.toppings = []
+        else:
+            self.toppings = toppings
+
+    @property
+    def day(self):
+        return self.__day
+
+    @day.setter
+    def day(self, value):
+        if not isinstance(value, str):
+            raise TypeError
+        if value not in {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}:
+            raise ValueError
+        self.__day = value
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError
+        self.__name = value
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        self.__price = value
+
+    @property
+    def ingredients(self):
+        return self.__ingredients
+
+    @ingredients.setter
+    def ingredients(self, value):
+        if not all([isinstance(v, str) for v in value]):
+            raise TypeError
+        self.__ingredients = value
+
+    @property
+    def toppings(self):
+        return self.__toppings
+
+    @toppings.setter
+    def toppings(self, value):
+        if not all([isinstance(v, str) for v in value]):
+            raise TypeError
+        with open("pizza_topings.json", 'r') as file:
+            toppings = json.load(file)
+        if not all([v in toppings for v in value]):
+            raise ValueError
+        self.__toppings = value
+
+    def prices(self):
+        """
+        Price pizza
+        :return: returns a whole pizza with extra ingredients
+        """
+        price = 0
+        with open("pizza_topings.json", 'r') as file:
+            topp = json.load(file)
+        for top in self.toppings:
+            for t in topp:
+                if top == t:
+                    price += topp[t]
+        return self.price + price
+
+    def add_ingredient(self, value):
+        """
+        Adds ingredients to pizza
+        :param value: new ingredients
+        """
+        if not all([isinstance(v, str) for v in value]):
+            raise TypeError
+        with open("pizza_topings.json", 'r') as file:
+            toppings = json.load(file)
+        if not all([v in toppings for v in value]):
+            raise ValueError
+        for v in value:
+            self.toppings.append(v)
+
+    def del_ingredient(self, value):
+        """
+        Removes ingredients from pizza
+        :param value: del ingredients
+        """
+        if value in self.toppings:
+            self.toppings.remove(value)
+        elif value in self.ingredients:
+            self.ingredients.remove(value)
+        else:
+            raise ValueError
+
+    def __str__(self):
+        if len(self.toppings):
+            return f'{self.name} {self.ingredients} add ingredients: {self.toppings}\n'
+        return f'{self.name} {self.ingredients}\n'
+
+
+class PizzaMonday(PizzaOfTheDay):
+    """
+    Pizza Monday
+    """
+    def __init__(self, day, pizza_of_a_day, price, ingredients, toppings=None):
+        super().__init__(day, pizza_of_a_day, price, ingredients, toppings)
+
+
+class PizzaTuesday(PizzaOfTheDay):
+    """
+    Pizza Tuesday
+    """
+    def __init__(self, day, pizza_of_a_day, price, ingredients, toppings=None):
+        super().__init__(day, pizza_of_a_day, price, ingredients, toppings)
+
+
+class PizzaWednesday(PizzaOfTheDay):
+    """
+    Pizza Wednesday
+    """
+    def __init__(self, day, pizza_of_a_day, price, ingredients, toppings=None):
+        super().__init__(day, pizza_of_a_day, price, ingredients, toppings)
+
+
+class PizzaThursday(PizzaOfTheDay):
+    """
+    Pizza Thursday
+    """
+    def __init__(self, day, pizza_of_a_day, price, ingredients, toppings=None):
+        super().__init__(day, pizza_of_a_day, price, ingredients, toppings)
+
+
+class PizzaFriday(PizzaOfTheDay):
+    """
+    Pizza Friday
+    """
+    def __init__(self, day, pizza_of_a_day, price, ingredients, toppings=None):
+        super().__init__(day, pizza_of_a_day, price, ingredients, toppings)
+
+
+class PizzaSaturday(PizzaOfTheDay):
+    """
+    Pizza Saturday
+    """
+    def __init__(self, day, pizza_of_a_day, price, ingredients, toppings=None):
+        super().__init__(day, pizza_of_a_day, price, ingredients, toppings)
+
+
+class PizzaSunday(PizzaOfTheDay):
+    """
+    Pizza Sunday
+    """
+    def __init__(self, day, pizza_of_a_day, price, ingredients, toppings=None):
+        super().__init__(day, pizza_of_a_day, price, ingredients, toppings)
+
+
+class Order:
+    """
+    Class order
+    Attributes
+    __________
+    pizzas: Pizza
+        array of pizzas
+    customer: str
+        customer
+
+    Methods
+    _______
+    add():
+        adds new ingredients
+    dell():
+        removes ingredients
+    all_pizzas():
+        returns pizza
+    write_date():
+        writes data to json
+    """
+    def __init__(self, pizza, customer):
+        """
+
+        :param pizza:
+            pizzas
+        :param customer:
+            customer
+        """
+        self.pizzas = pizza
+        self.customer = customer
+
+    @property
+    def pizzas(self):
+        return self.__pizzas
+
+    @pizzas.setter
+    def pizzas(self, value):
+        if not all([isinstance(v, PizzaOfTheDay) for v in value]):
+            raise TypeError
+        self.__pizzas = value
+
+    @property
+    def customer(self):
+        return self.__customer
+
+    @customer.setter
+    def customer(self, value: str):
+        self.__customer = value
+
+    @property
+    def order_amount(self):
+        price = 0
+        for pizza_day in self.pizzas:
+            price = price + pizza_day.prices()
+        return price
+
+    def add(self, value, number=1):
+        """
+        Adds ingredients
+        :param value:
+            new ingredients
+        :param number:
+            pizza to which you need to add ingredients
+        """
+        if not isinstance(number, int):
+            raise TypeError
+        if number > len(self.pizzas) or number < 0:
+            raise ValueError
+        self.pizzas[number - 1].add_ingredient(value)
+
+    def dell(self, value, number=1):
+        """
+        Removes ingredient
+        :param value:
+            ingredient
+        :param number:
+            pizza from which you need to remove the ingredients
+        """
+        if not isinstance(number, int):
+            raise TypeError
+        if number > len(self.pizzas) or number < 0:
+            raise ValueError
+        self.pizzas[number - 1].del_ingredient(value)
+
+    def all_pizzas(self):
+        """
+        All pizzas
+        :return: mas pizza
+        """
+        return "\n".join(map(str, self.pizzas))
+
+    def write_date(self):
+        """
+        Writes data
+        """
+        with open("order.json", 'r') as file:
+            orders = json.load(file)
+            orders.append({"customer": self.customer, "day": self.pizzas[0].day,
+                           "pizza": self.pizzas[0].name, "price": self.order_amount})
+        with open("order.json", 'w') as file:
+            json.dump(orders, file, indent=4)
+
+    def __str__(self):
+        return f'Customer: {self.customer} \nPizza: {self.all_pizzas()}To pay: {self.order_amount}'
+
+
+def pizza_of_the_day(day):
+    """
+    Returns the pizza of the day
+    :param day:
+        today
+    :return: pizza of the day
+    """
+    with open("pizza_of_the_day.json", 'r') as file:
+        pizza_days = json.load(file)
+    for pizza_day in pizza_days:
+        if pizza_day["day"] == day:
+            if day == "Monday":
+                return PizzaMonday(pizza_day["day"], pizza_day["name"], pizza_day["price"], pizza_day["ingredients"])
+            if day == "Tuesday":
+                return PizzaTuesday(pizza_day["day"], pizza_day["name"], pizza_day["price"], pizza_day["ingredients"])
+            if day == "Wednesday":
+                return PizzaWednesday(pizza_day["day"], pizza_day["name"], pizza_day["price"], pizza_day["ingredients"])
+            if day == "Thursday":
+                return PizzaThursday(pizza_day["day"], pizza_day["name"], pizza_day["price"], pizza_day["ingredients"])
+            if day == "Friday":
+                return PizzaFriday(pizza_day["day"], pizza_day["name"], pizza_day["price"], pizza_day["ingredients"])
+            if day == "Saturday":
+                return PizzaSaturday(pizza_day["day"], pizza_day["name"], pizza_day["price"], pizza_day["ingredients"])
+            if day == "Sunday":
+                return PizzaSunday(pizza_day["day"], pizza_day["name"], pizza_day["price"], pizza_day["ingredients"])
+    print("Incorrect data")
+    return None
+
+
+pizza_today = []
+
+try:
+    name_customer = input("Enter name: ")
+    num = int(input("How many pizzas do you want: "))
+    for i in range(num):
+        pizza_today.append(pizza_of_the_day(datetime.today().strftime('%A')))
+    x = Order(pizza_today, name_customer)
+    print(x.pizzas[0])
+    while True:
+        mod = int(input("1 - add ingredient\n2 - del_ingredient\n3 - exit\nEnter:"))
+        if mod == 1:
+            mass = []
+            ind_ingredients = int(input("Enter the number of ingredients you want to add: "))
+            ind_pizza = int(input("Which pizza do you want to add ingredients to: "))
+            print("Enter ingredients: ")
+            for i in range(ind_ingredients):
+                mass.append(str(input()))
+            x.add(mass, ind_pizza)
+        if mod == 2:
+            ingredient = str(input("Enter the ingredient you want to remove: "))
+            ind_pizza = int(input("Which pizza do you want to dell ingredients to: "))
+            x.dell(ingredient, ind_pizza)
+        if mod == 3:
+            print(x)
+            x.write_date()
+            exit(1)
+except  FileNotFoundError:
+    print("Error")
